eed6d44552cfb8b0ca7a6804fd4795b24684cfaf
Rainthee34 <juanpabloosorio824@gmail.com>
2025-05-01 22:51:29 -0500

    Add files via upload

---
+++ Biggest_value.py
@@ -0,0 +1,21 @@
+'''
+
+                            Online Python Debugger.
+                Code, Run and Debug Python program online.
+Write your code in this editor and press "Debug" button to debug program.
+
+'''
+# Get the biggest value
+
+# variables
+lista = [4, 20.1, 200, 13]
+unomas = 0
+
+# Loops
+for mayor in lista:
+   if mayor > unomas:
+      unomas = mayor
+      
+print(unomas)
+
+          
